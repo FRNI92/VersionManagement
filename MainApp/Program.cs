f8c17456c71c80d16a5d0b2ff9d47f8f71ad1875
fredrik <freedrik92@hotmail.com>
2024-11-18 15:25:30 +0100

    diaog added

--- MainApp/Program.cs
+++ MainApp/Program.cs
@@ -1,2 +1,8 @@
-﻿
-Console.WriteLine("Hejsan");
+﻿Console.WriteLine("Hejsan och välkommern");
+Console.WriteLine("Var vänligen och fyll i informationen här nedan.");
+
+Console.Write("Förnamn:");
+    string firstName = Console.ReadLine()!;
+
+Console.Write("Efternamn: ");
+    string lastName = Console.ReadLine()!;
