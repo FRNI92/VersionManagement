f67da5fbb82c30d4af374d7eddfd32d5016c9841
fredrik <freedrik92@hotmail.com>
2024-11-20 15:02:40 +0100

    La till en do-while och en list som jag kan skriva ut med foreach när användaren skriver "avsluta"

---
+++ Contact_Information_Consol/Program.cs
@@ -0,0 +1,44 @@
+﻿/*  Fråga om kontaktuppgifter
+    Namn?
+    kontrollera namn
+    ålder?
+    spara ålder
+    skriv ut ålder
+*/
+using System.Text.RegularExpressions;
+List<string> userInputList = [];
+string userNameInput;
+Regex nameCheck = new Regex(@"^[a-zA-Z]+$");
+
+do 
+    {
+    Console.Write("Vi behöver få in dina kontaktuppgifte\n skriv in ditt namn eller skriv 'Avsluta' för att stänga fönstret:");
+     userNameInput = Console.ReadLine();
+
+        if (userNameInput.ToLower() == "avsluta")
+        {
+            break;
+        }
+    
+        if (nameCheck.IsMatch(userNameInput))
+        {
+            userInputList.Add(userNameInput);
+            Console.WriteLine($"Vi har lagt till: {userNameInput}");
+        }
+        else
+        {
+            Console.WriteLine("You need to enter a valid name. no numbers ");
+        }
+    } while (true);
+
+Console.WriteLine("\nKontaktuppgifter");
+
+foreach (string userInputName in userInputList)
+{
+    Console.WriteLine($"- {userInputName}");
+}
+
+Console.ReadKey();
+
+
+
