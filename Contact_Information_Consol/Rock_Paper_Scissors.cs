265e7a63b1949df974bbc2fc1c379dfa2674143e
fredrik <freedrik92@hotmail.com>
2024-11-21 16:49:48 +0100

    har lagt till en räknare som ska hålla koll på hur många gånger man valt rock"

--- Contact_Information_Consol/Rock_Paper_Scissors.cs
+++ Contact_Information_Consol/Rock_Paper_Scissors.cs
@@ -11,21 +11,32 @@ namespace Contact_Information_Consol //tillhör samma namespace som resten av pr
     {
         public static void Play()// för att vara tillgänglig
         {
+
+                
+
+            List<string> hansHandSign = [];
+            List<string> userHandSign = [];
+
+            
+
             //Console.WriteLine("Välkommen till spelet, det finns inte än men är på G");
 
             bool continuefightingHans = true;// håller kolla om vi ska forsätta spela
-            var rock = "1";
-            var scissors = "2";
-            var paper = "3";
+            //var rock = "1"; gör om detta till en array
+            //var scissors = "2";
+            //var paper = "3";
+            string[] hands = {  "rock", "scissors", "paper" }; // innehåller fasta värden, ej dynamisk som List
             do
             {
-                Console.WriteLine("write Rock, Paper or Scissors. skriv klar för att avsluta");
-                var userHand = Console.ReadLine().ToLower();
+                Console.WriteLine("write Rock, Paper or Scissors.");
+                var userHand = Console.ReadLine().ToLower();//tar input och gör det till små bokstäver
 
-                Random random = new Random();
-                var computerHans = random.Next(1, 4);
+                Random random = new Random(); //kallar på en inbyggd class. kommer från using. men den "väcks" till liv med new
+                var computerHans = random.Next(1, 4); // 1/2/3 genererar något av dessa tal. en method i randomklassen som tar argument så som 1 och 4
+                string hansHand = hands[computerHans - 1]; //rock har index 0 så vi drar bort med -1. kanske kan priva att ändra random.next från 0-3 men jag har andra problem
 
-                if ((userHand == "rock" && computerHans == 2) ||
+                //jämför användarens "gissining" med datorn
+                if ((userHand == "rock" && computerHans == 2) ||// AND och OR operander
                 (userHand == "scissors" && computerHans == 3) ||
                 (userHand == "paper" && computerHans == 1))
                 {
@@ -48,16 +59,27 @@ namespace Contact_Information_Consol //tillhör samma namespace som resten av pr
                 {
                     Console.WriteLine("du måste använda näven!");
                 }
-
+                userHandSign.Add(userHand);//add är en method i List
+                hansHandSign.Add(hansHand);
+                Console.WriteLine($"\tDu valde { userHand }\n\tHans valde: { hansHand }");
                 Console.WriteLine("orkar du spela mer? (ja/nej)");
                 string answer = Console.ReadLine().ToLower();
                 if (answer == "nej")
                 {
                     continuefightingHans = false;//avslutar fighten
+
                 }
+
             } while (continuefightingHans);
-
-
+            int userRock = 0;
+            for (int i = 0; i < userHandSign.Count; i++)// den här ska visa hur många gånger man valt en viss näve. kan utökas
+            {
+                if (userHandSign[i] == "rock")
+                {
+                    userRock++;
+                }
+            }
+            Console.WriteLine($"Du valde rock {userRock}: Gånger");
         }
     }
 }
