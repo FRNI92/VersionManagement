f7196e9755afa6341440f0dddbb153835f89dc01
fredrik <freedrik92@hotmail.com>
2024-11-29 20:04:26 +0100

    Har ändrat så att DateTime fungerar rätt nu. den får inte vara string

--- UserListManager/Program.cs
+++ UserListManager/Program.cs
@@ -2,30 +2,42 @@
 using System.Reflection.Metadata;
 using UserListManager.Services;
 using UserListManager.Models;
+
+/*skapa 3 classer, en för meny(gör den i program), en för filen(services) och en för users(models)
+ */
 class Program
 {
     static void Main()
     {
-        var fileSavingService = new FileSavingService();
-        var userTemplates = fileSavingService.LoadFromFile();
+        var fileSavingService = new FileSavingService();//skapar en instans av class FileSavingService(är ej skriven med static)
+        var userTemplates = fileSavingService.LoadFromFile();//sparar object från metoden LoadFromFile
+        /*Brevbäraren(Program) vill öppna dörren och få tillgång till en instans av maskinen(FileSavingServie 
+         * som kan hjälpa oss skriva ut rätt fil med metoden LoadFromFile*/
 
-        while (true) 
+        while (true) //den här bestämmer om vi ska springe genom kodblocket igen eller ej
         {
-            Console.Clear();
+            Console.Clear();//rensar det som varit tidigare i loopen
             Console.WriteLine("1. Add a user");
             Console.WriteLine("2. Show all users");
             Console.WriteLine("3. Exit");
 
-            var choiceInput = Console.ReadLine();
+            var choiceInput = Console.ReadLine();//var verkar inte klaga på null i jämförelse med string
 
-            switch (choiceInput)
+            switch (choiceInput)//beroende på vad man klickat i så åker vi till rätt case
             {
                 case "1":
+                    /*här öppnar vi inte dörren till classen UserTemplate. vi använder konstruktor med samma namn
+                     för att direkt vi using.models
+                    vi skapar objectet med new. men använder using för att se mallen.
+                    using är hur brevet ska vara utformat, vilken adress, avsändare. frimärke med stämpel eller ej
+                    medans new userTemplate skapar det faktiska breve utifrån mallen.
+                    finns ingen mall så måste vi säga exakt hur den ska se ut
+                    */
                     Console.WriteLine("Submit you name");
                     var nameInput = Console.ReadLine();
                     Console.WriteLine("Submit you email");
                     var emailInput = Console.ReadLine();
-                    userTemplates.Add(new UserTemplate (nameInput, emailInput));
+                    userTemplates.Add(new UserTemplate (nameInput, emailInput));//är samma som userTemplates.Add(new UserListManager.Models.UserTemplate(nameInput, emailInput));
                     fileSavingService.SaveToFile(userTemplates);
                     Console.WriteLine("The user has been added");
                     break;
--- UserListManager/Services/FileSavingService.cs
+++ UserListManager/Services/FileSavingService.cs
@@ -1,23 +1,21 @@
 ﻿using System.Text.Json;
 using UserListManager.Models;
 
-namespace UserListManager.Services
+namespace UserListManager.Services;
+public class FileSavingService
 {
-    public class FileSavingService
+    public string filePath = "user.json";
+
+    public void SaveToFile(List<UserTemplate> UserTemplates)
     {
-        public string filePath = "user.json";
+        var json = JsonSerializer.Serialize(UserTemplates, new JsonSerializerOptions { WriteIndented = true });
+        File.WriteAllText(filePath, json);
+    }
 
-        public void SaveToFile(List<UserTemplate> UserTemplates)
-        {
-            var json = JsonSerializer.Serialize(UserTemplates, new JsonSerializerOptions { WriteIndented = true });
-            File.WriteAllText(filePath, json);
-        }
-
-        public List<UserTemplate> LoadFromFile() 
-        {
-            if (!File.Exists(filePath)) return new List<UserTemplate>();
-            var json = File.ReadAllText(filePath);
-            return JsonSerializer.Deserialize<List<UserTemplate>>(json) ?? new List<UserTemplate>();
-        }
+    public List<UserTemplate> LoadFromFile() 
+    {
+        if (!File.Exists(filePath)) return new List<UserTemplate>();
+        var json = File.ReadAllText(filePath);
+        return JsonSerializer.Deserialize<List<UserTemplate>>(json) ?? new List<UserTemplate>();
     }
 }
